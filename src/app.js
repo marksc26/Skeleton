e1efd344f48fd952657f37c09e48a2bf3cdb90d1
*Marcos <maromasi@hotmail.com>
2023-02-24 16:46:28 -0600

    firts commit

--- src/app.js
+++ src/app.js
@@ -3,11 +3,10 @@ const express = require('express')
 const responseHandlers = require('./utils/handleResponses')
 const db = require('./utils/database')
 const initModels = require('./models/initModels')
-const passportJwt = require('./middlewares/auth.middleware')
+
 
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
-const conversationRouter = require('./conversations/conversations.router')
 
 const app = express()
 
@@ -30,23 +29,16 @@ app.get('/', (req, res) => {
         message: 'Servidor inicializado correctamente',
         data: {
             "users": "http://localhost:9000/api/v1/users",
-            "conversations": "http://localhost:9000/api/v1/conversations"
+            
         }
     })
 })
 
-app.get('/protected', 
-    passportJwt,
-    (req, res) => {
-        res.status(200).json({
-            message: `Hola ${req.user.firstName} este mensaje solo lo puedes ver si tienes sesion iniciada! :D`,
-        })
-    }
-)
+
 
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/conversations', conversationRouter)
+
 
 //? Esta debe ser la ultima ruta en mi app
 app.use('*', (req, res)=> {
